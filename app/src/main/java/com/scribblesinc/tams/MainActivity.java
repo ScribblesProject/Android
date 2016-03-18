09eb94e60a4b58aa866691bada3a006e8df52db1
Alejandro Peña <alejandropena16@yahoo.com>
2016-03-18 10:28:00 -0700

    Added Map Fragment

--- app/src/main/java/com/scribblesinc/tams/MainActivity.java
+++ app/src/main/java/com/scribblesinc/tams/MainActivity.java
@@ -1,13 +1,13 @@
 package com.scribblesinc.tams;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.maps.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         //setTitle(null);
 
