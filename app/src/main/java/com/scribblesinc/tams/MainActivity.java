89b3d70b1bfc9713688551f5c7d429d5c0d472ea
Alejandro Peña <alejandropena16@yahoo.com>
2016-04-13 09:49:23 -0700

    Added Location Permission Checks.

--- app/src/main/java/com/scribblesinc/tams/MainActivity.java
+++ app/src/main/java/com/scribblesinc/tams/MainActivity.java
@@ -1,13 +1,9 @@
 package com.scribblesinc.tams;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -33,13 +29,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleApiClient mGoogleApiClient;
     private Toolbar toolbar;
 
-    private double latitude, longitude;
-
-    private MyLocationListener locationListener;
-
-    /* Request constants used for permission reasons. */
-    private static final int REQUEST_LOCATION_RESULT = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,12 +51,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setSupportActionBar(toolbar);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Connect the client.
-        mGoogleApiClient.connect();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -92,6 +75,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     Toast.LENGTH_SHORT).show();
         }
 
+        //if the current location button is pressed
+        if(id == R.id.action_current_location){
+            Toast.makeText(getApplicationContext(), "Not Working Yet",
+                    Toast.LENGTH_SHORT).show();
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -100,78 +89,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Hard-code marker by CSUS to test maps
-        //LatLng csus = new LatLng(38.559144, -121.4256621);
-        //mMap.addMarker(new MarkerOptions().position(csus).title("CSUS")).setVisible(true);
+        LatLng csus = new LatLng(38.559144, -121.4256621);
+        mMap.addMarker(new MarkerOptions().position(csus).title("CSUS")).setVisible(true);
 
         // Move the camera instantly to location with a zoom of 15.
-        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(csus, 15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(csus, 15));
 
         // Zoom in, animating the camera.
-        //mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
     }
 
-    //this method is called when the current location menu item is tapped
-    public void currentLocationAction(MenuItem item){
-        //Toast.makeText(getApplicationContext(), "Location Not Yet Working", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onConnected(Bundle bundle) {
+        //Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
-        if(Build.VERSION.SDK_INT >= 23){ //6.0+
-            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                //calculate current location
-                getCurrentLocation();
-                //Toast.makeText(getApplicationContext(), "Locating Permission Granted", Toast.LENGTH_SHORT).show();
-            }else {
-                //rejected permission request
-                if(shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)){
-                    //Toast.makeText(getApplicationContext(), "Location Permission Required", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "Location Permission Required");
-                }
-                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_RESULT);
-            }
-        }else{ // < 6.0
-            //Toast.makeText(getApplicationContext(), "Location Permission Granted", Toast.LENGTH_SHORT).show();
-            getCurrentLocation();
+        /*if (location == null) {
+            // Blank for a moment...
+
         }
-    }
-
-    public void getCurrentLocation(){
-
-
-        /*locationListener = new MyLocationListener(MainActivity.this);
-
-        mMap.clear();
-
-        if(locationListener.canGetLocation){
-            this.latitude = locationListener.getLatitude();
-            this.longitude = locationListener.getLongitude();
-            LatLng currentLocation = new LatLng(latitude, longitude);
-            mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location")).setVisible(true);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
-            mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
-
-
-            Toast.makeText(MainActivity.this, "Location: \nLat: " + latitude + "\nLong: "
-                    + longitude, Toast.LENGTH_LONG).show();
+        else {
+            handleNewLocation(location);
         }*/
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-        if(requestCode == REQUEST_LOCATION_RESULT){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                //calculate current location
-                getCurrentLocation();
-            } else {
-                Toast.makeText(getApplicationContext(), "Location Permission Required", Toast.LENGTH_SHORT).show();
-
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Toast.makeText(MainActivity.this, "Map Connected", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -181,6 +119,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void handleNewLocation(Location location) {
 
     }
 }
